dc4a3a88b1edae0e4f96ea12bb2ebceed8aa8c62
Tim Creech <tcreech@umd.edu>
2015-07-29 23:37:12 -0400

    restore kernel module for SunOS
    Still needed:
    * Makefile needs to be smarter about installing kernel module
    * Port could automatically generate SMF manifests, e.g., based on [1].
    * Testing
    
    1. Only client is ready as of now: http://github.com/tcreech/openafs-smf

--- filesystems/openafs/options.mk
+++ filesystems/openafs/options.mk
@@ -1,13 +1,11 @@
-# $NetBSD: options.mk,v 1.3 2014/02/21 21:33:51 gendalia Exp $
-
 .include "../../mk/bsd.prefs.mk"
 
 PKG_OPTIONS_VAR=        PKG_OPTIONS.openafs
-PKG_SUPPORTED_OPTIONS+= server bitmap-later kernel-module supergroups namei
+PKG_SUPPORTED_OPTIONS+= server bitmap-later kernel-module supergroups namei rename-kpasswd
 .if ${MACHINE_ARCH} != "x86_64"
 PKG_SUPPORTED_OPTIONS+= pam
 .endif
-PKG_SUGGESTED_OPTIONS=  server -bitmap-later namei -kernel-module supergroups
+PKG_SUGGESTED_OPTIONS=  server -bitmap-later namei -kernel-module supergroups rename-kpasswd
 
 .include "../../mk/bsd.options.mk"
 
@@ -19,6 +17,7 @@ CONFIGURE_ARGS+=        --enable-bitmap-later
 .endif
 .if !empty(PKG_OPTIONS:Mkernel-module)
 CONFIGURE_ARGS+=        --enable-kernel-module
+PLIST_SRC+=				${.CURDIR}/PLIST.kernel
 .else
 CONFIGURE_ARGS+=        --disable-kernel-module
 .endif
@@ -31,4 +30,9 @@ CONFIGURE_ARGS+=        --enable-pam
 PLIST_SRC+=             ${.CURDIR}/PLIST.pam
 .else
 CONFIGURE_ARGS+=        --disable-pam
+.endif
+.if !empty(PKG_OPTIONS:Mrename-kpasswd)
+PLIST_SRC+=				${.CURDIR}/PLIST.rename-kpasswd
+.else
+PLIST_SRC+=				${.CURDIR}/PLIST.no-rename-kpasswd
 .endif
